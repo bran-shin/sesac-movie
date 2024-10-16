4201b4386a84d4fa6bb1a292afbee995596f4d31
Lets <ruvan123@naver.com>
2024-10-16 14:14:40 +0900

    TemporaryView 구현

---
+++ movie/movie/TemporaryView.swift
@@ -0,0 +1,14 @@
+//
+//  TemporaryView.swift
+//  movie
+//
+//  Created by user on 10/16/24.
+//
+
+import SwiftUI
+
+struct TemporaryView: View {
+    var body: some View {
+        Text("안녕 세상아")
+    }
+}
