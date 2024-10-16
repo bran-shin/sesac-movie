749e72ed5911c834800c3ca87ff8860deffeb2a9
srkimdev <emirated3858@gmail.com>
2024-10-16 14:14:46 +0900

    exampleView 구현

---
+++ movie/movie/exampleView.swift
@@ -0,0 +1,18 @@
+//
+//  exampleView.swift
+//  movie
+//
+//  Created by 김성률 on 10/16/24.
+//
+
+import SwiftUI
+
+struct exampleView: View {
+    var body: some View {
+        Text("Hi")
+    }
+}
+
+#Preview {
+    exampleView()
+}
