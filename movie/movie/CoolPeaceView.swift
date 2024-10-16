fd334cf122d441588e9a5483619d27711615907f
Timmy <80156515+bdrsky2010@users.noreply.github.com>
2024-10-16 14:14:45 +0900

    쿨피스뷰 구현

---
+++ movie/movie/CoolPeaceView.swift
@@ -0,0 +1,18 @@
+//
+//  CoolPeaceView.swift
+//  movie
+//
+//  Created by Minjae Kim on 10/16/24.
+//
+
+import SwiftUI
+
+struct CoolPeaceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoolPeaceView()
+}
