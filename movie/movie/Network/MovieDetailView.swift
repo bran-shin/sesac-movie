76f055ac7550ee151dc03c24d394f4c3fd18b396
Acasiax <117105267+Acasiax@users.noreply.github.com>
2024-10-16 14:26:34 +0900

    미디어 디테일뷰 구현

---
+++ movie/movie/Network/MovieDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  MovieDetailView.swift
+//  movie
+//
+//  Created by 이윤지 on 10/16/24.
+//
+
+import SwiftUI
+
+struct MediaDetailView: View {
+    
+    var body: some View {
+        Text("sdfdsfd")
+        Text("sdfdsfd")
+        
+    }
+    
+}
